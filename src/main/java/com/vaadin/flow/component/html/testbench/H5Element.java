adb2bf8a983def75f1721a1ba7884acbe6fa9b0a
Artur Signell <artur@vaadin.com>
2018-01-24 22:59:54 +0200

    HTML elements

---
+++ src/main/java/com/vaadin/flow/component/html/testbench/H5Element.java
@@ -0,0 +1,9 @@
+package com.vaadin.flow.component.html.testbench;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("h5")
+public class H5Element extends TestBenchElement {
+
+}
