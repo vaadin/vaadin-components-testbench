514c510af84096ddc9c7581d8c972092371893f1
Artur Signell <artur@vaadin.com>
2018-01-24 23:18:29 +0200

    Checkbox support

---
+++ src/main/java/com/vaadin/flow/component/checkbox/testbench/CheckboxElement.java
@@ -0,0 +1,32 @@
+package com.vaadin.flow.component.checkbox.testbench;
+
+import com.vaadin.flow.component.html.testbench.InputElement;
+import com.vaadin.flow.component.html.testbench.LabelElement;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("vaadin-checkbox")
+public class CheckboxElement extends TestBenchElement {
+    public boolean isChecked() {
+        return getPropertyBoolean("checked");
+    }
+
+    public void setChecked(boolean checked) {
+        setProperty("checked", checked);
+    }
+
+    public String getLabel() {
+        return (String) executeScript(
+                "return arguments[0].firstChild ? arguments[0].firstChild.textContent : '';",
+                this);
+    }
+
+    protected LabelElement getLabelElement() {
+        return getPropertyElement("$", "label").wrap(LabelElement.class);
+    }
+
+    protected InputElement getInputElement() {
+        return $(InputElement.class).attribute("part", "native-checkbox")
+                .first();
+    }
+}
