47192b5f8d66bcc2c50a45ec692cfeedfb504169
Artur Signell <artur@vaadin.com>
2018-01-24 22:59:54 +0200

    HTML elements

---
+++ src/main/java/com/vaadin/flow/component/html/testbench/SpanElement.java
@@ -0,0 +1,9 @@
+package com.vaadin.flow.component.html.testbench;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("span")
+public class SpanElement extends TestBenchElement {
+
+}
